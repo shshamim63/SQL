4980b03367a4e920bef4da02cae255d72cbee02a
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 19:38:33 +0600

    nobel database no.2

--- Queries/Select_From_Nobel.sql
+++ Queries/Select_From_Nobel.sql
@@ -1,5 +1,9 @@
 -- 1. It displays Nobel prizes for 1950.
 SELECT yr, subject, winner FROM nobel
-    WHERE yr = 1950;
+  WHERE yr = 1950;
 
 -- 2. Show who won the 1962 prize for Literature.
+SELECT winner FROM nobel
+  WHERE yr = 1962 AND subject = 'Literature';
+
+  
