c441c96dd436d00acc7cb271935b7d7b700297ab
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 22:49:18 +0600

    self_join no.7

--- Queries/Self_join.sql
+++ Queries/Self_join.sql
@@ -31,3 +31,9 @@ FROM route a JOIN route b ON
   JOIN stops stopb ON (b.stop = stopb.id)
 WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'London Road';
 
+-- 7. Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b 
+ON (a.company = b.company AND a.num = b.num)
+WHERE a.stop = 115 AND b.stop = 137;
+
