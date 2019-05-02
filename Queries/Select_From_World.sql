ba06235dd34346ef1a6332ddc6b99363cda132a5
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 17:02:01 +0600

    done with the second one

--- Queries/Select_From_World.sql
+++ Queries/Select_From_World.sql
@@ -2,3 +2,5 @@
 SELECT name, continent, population FROM world;
 
 -- 2. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+SELECT name FROM world
+WHERE population >= 200000000;
