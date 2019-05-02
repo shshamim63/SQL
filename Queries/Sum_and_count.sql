36ebd847e35c765955163382668fb2adf26475a9
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 22:07:19 +0600

    sum and count no.3

--- Queries/Sum_and_count.sql
+++ Queries/Sum_and_count.sql
@@ -3,3 +3,7 @@ SELECT SUM(population) FROM world
 
 -- 2. List all the continents - just once each.
 SELECT DISTINCT continent FROM world;
+
+-- 3. Give the total GDP of Africa
+SELECT SUM(gdp) FROM world
+WHERE continent = 'Africa';
