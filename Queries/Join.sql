370204818e6b866a461107a787edecc53ada6be2
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 22:21:55 +0600

    join no. 4-5

--- Queries/Join.sql
+++ Queries/Join.sql
@@ -11,3 +11,13 @@ WHERE id = 1012;
 SELECT player, teamid, stadium, mdate FROM game 
 JOIN goal ON (id = matchid)
 WHERE teamid = 'GER';
+
+-- 4. Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+SELECT team1, team2, player FROM game 
+JOIN goal ON (id = matchid)
+WHERE player LIKE 'Mario%';
+
+-- 5. Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON (teamid = id)
+WHERE gtime <= 10;
