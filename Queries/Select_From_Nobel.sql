0f9ae3fb5d7d8a5b42fb39e935402f1b97b3adf1
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 20:27:24 +0600

    nobel database no.14

--- Queries/Select_From_Nobel.sql
+++ Queries/Select_From_Nobel.sql
@@ -28,26 +28,31 @@ SELECT winner FROM nobel
 
 -- 8. Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
 SELECT yr, subject, winner FROM nobel
-WHERE (subject = 'Physics' AND yr = 1980) OR (subject = 'Chemistry' AND yr = 1984);
+  WHERE (subject = 'Physics' AND yr = 1980) OR (subject = 'Chemistry' AND yr = 1984);
 
 -- 9.Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
 SELECT yr, subject, winner FROM nobel
-WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
+  WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
 
 -- 10. Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910)
 -- together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
 SELECT yr, subject, winner FROM nobel
-WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
+  WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
 
 -- 11. Find all details of the prize won by PETER GRÜNBERG
 SELECT * FROM nobel
-WHERE winner = 'PETER GRÜNBERG';
+  WHERE winner = 'PETER GRÜNBERG';
 
 -- 12. Find all details of the prize won by EUGENE O'NEILL
 SELECT * FROM nobel
-WHERE winner = 'EUGENE O''NEILL';
+  WHERE winner = 'EUGENE O''NEILL';
 
 -- 13. List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
 SELECT winner, yr, subject FROM nobel
-WHERE winner LIKE 'Sir%'
-ORDER BY yr DESC, winner;
+  WHERE winner LIKE 'Sir%'
+  ORDER BY yr DESC, winner;
+
+-- 14.Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+SELECT winner, subject FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN('Chemistry','Physics'),subject,winner;
