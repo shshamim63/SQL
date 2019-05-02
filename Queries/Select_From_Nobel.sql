b2a70ffaf50c5e29dc70538eae311a43bfa94097
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 19:43:46 +0600

    nobel database no.5

--- Queries/Select_From_Nobel.sql
+++ Queries/Select_From_Nobel.sql
@@ -15,3 +15,5 @@ SELECT winner FROM nobel
   WHERE subject = 'Peace' AND yr >= 2000;
 
 -- 5. Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+SELECT yr, subject, winner FROM nobel
+WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989;
