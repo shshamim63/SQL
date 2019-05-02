352c1675753bc4f33d07cb2679b7edbc7dc69380
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 17:20:15 +0600

    done with the no.4

--- Queries/Select_From_World.sql
+++ Queries/Select_From_World.sql
@@ -7,3 +7,8 @@ SELECT name FROM world
 -- 3.Give the name and the per capita GDP for those countries with a population of at least 200 million.
 SELECT name, gdp/population AS per_capita_GDP FROM world
   WHERE population >= 200000000;
+
+-- 4.Show the name and population in millions for the countries of the continent 'South America'. 
+--Divide the population by 1000000 to get population in millions.
+SELECT name, population/1000000 as population_in_millions FROM world
+  WHERE continent = 'South America';
