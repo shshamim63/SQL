ec2bdf0c48bcda48ef3a6eb03031ceb49be3740c
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 17:25:52 +0600

    done with the no.7

--- Queries/Select_From_World.sql
+++ Queries/Select_From_World.sql
@@ -22,3 +22,5 @@ SELECT name FROM world
   WHERE name LIKE '%United%';
 
 -- 7. Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name, population, area FROM world
+  WHERE area > 3000000 OR population > 250000000;
