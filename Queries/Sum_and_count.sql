906ab9eab0e8045fda87a48a70b691ab65a915c1
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 22:10:39 +0600

    sum and count no.4-5

--- Queries/Sum_and_count.sql
+++ Queries/Sum_and_count.sql
@@ -7,3 +7,11 @@ SELECT DISTINCT continent FROM world;
 -- 3. Give the total GDP of Africa
 SELECT SUM(gdp) FROM world
 WHERE continent = 'Africa';
+
+-- 4. How many countries have an area of at least 1000000
+SELECT COUNT(name) FROM world
+WHERE area >= 1000000;
+
+-- 5. What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+SELECT SUM(population) FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
