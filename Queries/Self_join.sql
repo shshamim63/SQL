ef8723b999d6fb665a986edc7d0672e39899d2e0
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 22:47:46 +0600

    self_join no.4

--- Queries/Self_join.sql
+++ Queries/Self_join.sql
@@ -9,3 +9,10 @@ WHERE name = 'Craiglockhart';
 SELECT id, name FROM route 
 JOIN stops ON stops.id =stop
 WHERE num = 4 AND company = 'LRT';
+
+-- 4. Give the number of routes that visit either London Road (149) or Craiglockhart (53). The two services that link these stops have a count of 2. 
+-- Restrict the output to these two routes.
+SELECT company, num, COUNT(*) FROM route
+WHERE stop = 149 OR stop = 53
+GROUP BY company, num
+HAVING COUNT(*) = 2;
