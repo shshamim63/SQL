2fd05d3affe9fb5b162c3b7932ee1683440762e2
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 22:43:59 +0600

    using null no.10

--- Queries/Using_null.sql
+++ Queries/Using_null.sql
@@ -34,3 +34,8 @@ GROUP BY dept.name;
 -- 9. Show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
 SELECT name, CASE WHEN dept = 1 OR dept = 2 THEN 'Sci' ELSE 'Art' END
 FROM teacher;
+
+-- 10. Show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' 
+-- if the teacher's dept is 3 and 'None' otherwise.
+SELECT name, CASE WHEN dept = 1 OR dept = 2 THEN 'Sci' WHEN dept = 3 THEN 'Art' ELSE 'None' END
+FROM teacher;
