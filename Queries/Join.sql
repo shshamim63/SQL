70bcc133778dd4fcfcc7f452e69987f2c157fe67
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 22:24:23 +0600

    join no. 6-13

--- Queries/Join.sql
+++ Queries/Join.sql
@@ -21,3 +21,53 @@ WHERE player LIKE 'Mario%';
 SELECT player, teamid, coach, gtime
 FROM goal JOIN eteam ON (teamid = id)
 WHERE gtime <= 10;
+
+-- 6. List the the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+SELECT mdate, teamname
+FROM game JOIN eteam ON eteam.id = team1
+WHERE coach = 'Fernando Santos';
+
+-- 7. List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
+SELECT player
+FROM goal JOIN game ON matchid = id
+WHERE stadium = 'National Stadium, Warsaw';
+
+-- 8. Show the name of all players who scored a goal against Germany.
+SELECT DISTINCT player
+FROM game JOIN goal ON matchid = id
+WHERE (team1 = 'GER'
+XOR team2 = 'GER') AND teamid <> 'GER';
+
+-- 9. Show teamname and the total number of goals scored.
+SELECT teamname, COUNT(teamid)
+FROM eteam JOIN goal ON id = teamid
+GROUP BY teamname;
+
+-- 10. Show the stadium and the number of goals scored in each stadium.
+SELECT stadium, COUNT(stadium)
+FROM game JOIN goal ON matchid = id
+WHERE teamid IN (team1, team2)
+GROUP BY stadium;
+
+-- 11. For every match involving 'POL', show the matchid, date and the number of goals scored.
+SELECT matchid, mdate, COUNT(teamid)
+FROM game JOIN goal ON matchid = id
+WHERE team1 = 'POL'
+XOR team2 = 'POL'
+GROUP BY matchid, mdate;
+
+-- 12. For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
+SELECT matchid, mdate, COUNT(teamid)
+FROM game JOIN goal ON matchid = id
+WHERE teamid = 'GER'
+GROUP BY matchid, mdate;
+
+-- 13. List every match with the goals scored by each team as shown.
+SELECT mdate,team1,
+  SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) score1,
+  team2,
+  SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) score2
+FROM game LEFT JOIN goal ON matchid = id
+GROUP BY mdate, team1, team2
+ORDER BY mdate, matchid, team1, team2;
+
