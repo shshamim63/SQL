74047849af70860d21fd360939b5bda5aaf2d294
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 20:59:43 +0600

    select in select no.6

--- Queries/Select_in_select.sql
+++ Queries/Select_in_select.sql
@@ -27,3 +27,8 @@ SELECT name, population FROM world
 SELECT name, CONCAT(ROUND(population/(SELECT population FROM world
   WHERE name = 'Germany') * 100), '%') AS Percentage_Germany FROM world
     WHERE continent = 'Europe';
+
+-- 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+SELECT name FROM world
+  WHERE gdp > ALL(SELECT gdp FROM world
+    WHERE continent = 'Europe' AND NOT gdp = 0);
