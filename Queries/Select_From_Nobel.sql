79912d17ca24c493784d64b889cbdb9025a31c13
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 19:53:08 +0600

    nobel database no.10

--- Queries/Select_From_Nobel.sql
+++ Queries/Select_From_Nobel.sql
@@ -33,3 +33,8 @@ WHERE (subject = 'Physics' AND yr = 1980) OR (subject = 'Chemistry' AND yr = 198
 -- 9.Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
 SELECT yr, subject, winner FROM nobel
 WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
+
+-- 10. Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910)
+-- together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+SELECT yr, subject, winner FROM nobel
+WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
