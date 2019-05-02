9e5125dafcb04dcee41349589070e263973a5677
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 19:43:20 +0600

    nobel database no.3 and 4

--- Queries/Select_From_Nobel.sql
+++ Queries/Select_From_Nobel.sql
@@ -6,4 +6,12 @@ SELECT yr, subject, winner FROM nobel
 SELECT winner FROM nobel
   WHERE yr = 1962 AND subject = 'Literature';
 
-  
+-- 3. Show the year and subject that won 'Albert Einstein' his prize.
+SELECT yr, subject FROM nobel
+  WHERE winner = 'Albert Einstein';
+
+-- 4. Give the name of the 'Peace' winners since the year 2000, including 2000.
+SELECT winner FROM nobel
+  WHERE subject = 'Peace' AND yr >= 2000;
+
+-- 5. Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
