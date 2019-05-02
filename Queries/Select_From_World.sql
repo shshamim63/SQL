f7d2bbd93807157d74f01aad0558dbe29cc491da
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-02 17:16:30 +0600

    done with the no.3

--- Queries/Select_From_World.sql
+++ Queries/Select_From_World.sql
@@ -3,4 +3,7 @@ SELECT name, continent, population FROM world;
 
 -- 2. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
 SELECT name FROM world
-WHERE population >= 200000000;
+  WHERE population >= 200000000;
+-- 3.Give the name and the per capita GDP for those countries with a population of at least 200 million.
+SELECT name, gdp/population AS per_capita_GDP FROM world
+  WHERE population >= 200000000;
